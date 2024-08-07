e7a9e33a6b8647f6c7800db8a254f2999b8cbd2c
jere <jere77lagos@gmail.com>
2024-08-07 15:09:08 +0200

    implement register form and some validations

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern)  ]],
+    username: ['', [ Validators.required, customValidators.cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]]
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  isValidField( field: string ) {
+    //todo: implement from a service
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched()
+  }
 
 }
